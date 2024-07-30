827681696155953b32542ce12372290aa3264b95
abalux <baluxagustina@gmail.com>
2024-07-30 16:09:11 -0300

    agregue la nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,50 @@
 import CartWidget from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
-function NavBar() {
+
+
+function NavBar({ cartCount, title }) {
     return (
-        <nav style={{display: "flex", justifyContent:"space-between", alignItems: 'center' }}>
-            <h1>Ecomerce</h1>
-            <section>
-            <Link to="/category/skirts"> Skirts </Link>
-            <Link to="/category/dresses"> Dresses </Link>
-            <Link to="/category/coats"> Coats </Link>
-            <Link to="/category/shirts"> Shirts </Link>
-            </section>
-            <CartWidget />
-        </nav>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid" >
+            <a className="navbar-brand" href="#">
+                {title}
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+                    <li className="nav-item ms-3">
+                        <Link to="/"> Home </Link>
+                    </li>
+                    <li className="nav-item ms-3">
+                        <Link to="/category/skirts"> Skirts </Link>
+                    </li>
+                    <li className="nav-item ms-3">
+                        <Link to="/category/dresses"> Dresses </Link>
+                    </li>
+                    <li className="nav-item ms-3">
+                        <Link to="/category/coats"> Coats </Link>
+                    </li>
+                    <li className="nav-item ms-3">
+                        <Link to="/category/shirts"> Shirts </Link>
+                    </li>
+                </ul>
+                <form className="d-flex" role="search">
+                    <CartWidget cartCount={cartCount} />
+                </form>
+            </div>
+        </div>
+    </nav>
     );
 }
 
